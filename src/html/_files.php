bdb50c80c5847eedaa894221a70c7d26a97c7aab
Maxim Zhgulev <max@8max.ru>
2019-03-14 14:53:28 +0300

    files improvements

--- src/html/_files.php
+++ src/html/_files.php
@@ -1,18 +1,26 @@
-<div class="files">
-    <a class="files__item" href="#" target="_blank">
-        <div class="files__item-title">Название какого-то файла</div>
-        <div class="files__item-download"><span class="icon-download"></span></div>
-    </a>
-    <a class="files__item" href="#" target="_blank">
-        <div class="files__item-title">Взаимодействие корпорации и клиента порождает побочный PR-эффект, работая над проектом.</div>
-        <div class="files__item-download"><span class="icon-download"></span></div>
-    </a>
-    <a class="files__item" href="#" target="_blank">
-        <div class="files__item-title">Инвестиционный продукт, осознавая социальную ответственность бизнеса.</div>
-        <div class="files__item-download"><span class="icon-download"></span></div>
-    </a>
-    <a class="files__item" href="#" target="_blank">
-        <div class="files__item-title">Стратегический рыночный план, на первый взгляд, индуцирует системный анализ, не считаясь с затратами.</div>
-        <div class="files__item-download"><span class="icon-download"></span></div>
-    </a>
-</div>
+<ul class="files">
+    <li class="files__item">
+        <a class="files__link" href="#" target="_blank">
+            <div class="files__download"><span class="icon-download"></span></div>
+            <div class="files__title">Название какого-то файла</div>
+        </a>
+    </li>
+    <li class="files__item">
+        <a class="files__link" href="#" target="_blank">
+            <div class="files__download"><span class="icon-download"></span></div>
+            <div class="files__title">Взаимодействие корпорации и клиента порождает побочный PR-эффект, работая над проектом.</div>
+        </a>
+    </li>
+    <li class="files__item">
+        <a class="files__link" href="#" target="_blank">
+            <div class="files__download"><span class="icon-download"></span></div>
+            <div class="files__title">Инвестиционный продукт, осознавая социальную ответственность бизнеса.</div>
+        </a>
+    </li>
+    <li class="files__item">
+        <a class="files__link" href="#" target="_blank">
+            <div class="files__download"><span class="icon-download"></span></div>
+            <div class="files__title">Стратегический рыночный план, на первый взгляд, индуцирует системный анализ, не считаясь с затратами.</div>
+        </a>
+    </li>
+</ul>

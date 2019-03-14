78d1381bd7605c973bd721c46d9ae47daf23c029
Maxim Zhgulev <max@8max.ru>
2019-03-14 14:49:11 +0300

    breadcrumbs improvements

--- src/html/_breadcrumbs.php
+++ src/html/_breadcrumbs.php
@@ -1,5 +1,11 @@
-<div class="breadcrumbs">
-    <a class="breadcrumbs__item" href="#" title="Главная">Главная</a>
-    <a class="breadcrumbs__item" href="#" title="Новости">Новости</a>
-    <a class="breadcrumbs__item" href="#" title="Новости спорта">Новости спорта</a>
-</div>
+<ul class="breadcrumbs">
+    <li class="breadcrumbs__item">
+        <a class="breadcrumbs__link" href="#" title="Главная">Главная</a>
+    </li>
+    <li class="breadcrumbs__item">
+        <a class="breadcrumbs__link" href="#" title="Новости">Новости</a>
+    </li>
+    <li class="breadcrumbs__item">
+        <a class="breadcrumbs__link" href="#" title="Новости спорта">Новости спорта</a>
+    </li>
+</ul>

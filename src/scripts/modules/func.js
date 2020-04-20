b4969274ede54b6e1240e915d14fed0bb884ea05
Maxim Zhgulev <max@8max.ru>
2020-04-20 22:10:18 +0300

    fix codestyle

--- src/scripts/modules/func.js
+++ src/scripts/modules/func.js
@@ -20,13 +20,12 @@ function scrollTo(target, duration, callback) {
 }
 
 /* Get element offset */
-function getElementOffset (element)
-{
-    var de = document.documentElement;
-    var box = element.getBoundingClientRect();
-    var top = box.top + window.pageYOffset - de.clientTop;
-    var left = box.left + window.pageXOffset - de.clientLeft;
-    return { top: top, left: left };
+function getElementOffset (element) {
+  var de = document.documentElement;
+  var box = element.getBoundingClientRect();
+  var top = box.top + window.pageYOffset - de.clientTop;
+  var left = box.left + window.pageXOffset - de.clientLeft;
+  return { top: top, left: left };
 }
 
 /* Show preloader */
